cd4c1b3a8354b365a243e5ba4f98a7fd0810e969
henrycunh <henrycunh@gmail.com>
2021-12-16 12:27:40 -0300

    added reproduction

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <input type="text" v-model="v$.name.$model">
+    <pre lang="json">
+      {{ v$ }}
+    </pre>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from '@nuxtjs/composition-api'
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+
+const state = reactive({ name: '' })
+const rules = { name: { required } }
+
+const v$ = useVuelidate(rules, state)
+</script>
